457bd563a54ddfa5c6de783cfe28a6ce6fb54843
nanis <nanis@AsusZPBDuo>
2022-05-03 14:25:19 +0530

    First Commit

---
+++ Assignment1/src/com/tns/operator/UnaryDecrement.java
@@ -0,0 +1,12 @@
+package com.tns.operator;
+public class UnaryDecrement {
+
+	public static void main(String[] args) {
+		int a=5,b=3;
+		int x=a--;
+		int y=--b;
+		int z=a-x;
+		System.out.println(z);
+	}
+
+}
