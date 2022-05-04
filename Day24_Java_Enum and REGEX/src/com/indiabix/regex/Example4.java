22a1e5ef7e89c83ea1295852e210c16a6bc47c59
nanis <nanis@AsusZPBDuo>
2022-05-04 18:09:44 +0530

    First Commit

---
+++ Day24_Java_Enum and REGEX/src/com/indiabix/regex/Example4.java
@@ -0,0 +1,20 @@
+package com.indiabix.regex;
+import java.util.Scanner;
+//import java.util.regex.Pattern;
+
+public class Example4 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		String str = "Batch 1-4 CG1.1 \r\nLokesh \r\nSumanth";
+		/*String delimiter = "\\d";
+		Pattern p = Pattern.compile(delimiter,Pattern.CASE_INSENSITIVE);*/
+		String[] res = str.split("\\r?\\n");
+		//System.out.println(res);
+		for(String i:res)
+		{
+			System.out.println(i);
+		}
+	}
+}
